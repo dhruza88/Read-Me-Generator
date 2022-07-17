dc048f468723d189f4b17f9616c97cd0987e3160
dhruza88 <davidhruza@gmail.com>
2022-07-17 09:52:44 -0700

    attempting to add badge

--- Develop/markdown.js
+++ Develop/markdown.js
@@ -1,12 +1,14 @@
 class mdclass {
 
-    // static createLicenseBadge(license){
-    //     const badges = [
-    //         mit:,
-    //         isc: ,
-    //         gnuplv3:
-    //         ]
-    // }
+    static createLicenseBadge(license){
+        const badges = 
+        {
+            mit: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+            isc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+            gnuplv3:'[![License: MIT](https://img.shields.io/badge/License-lgpl_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+            }
+            return badges[license]
+    }
     static geneReadme(ansr) {
         return `
         # ${ansr.title}
@@ -14,6 +16,7 @@ class mdclass {
 ## Description
 ${ansr.description}
 
+${this.createLicenseBadge(ansr.license)}
 
 ## Table of Contents (Optional)
 
